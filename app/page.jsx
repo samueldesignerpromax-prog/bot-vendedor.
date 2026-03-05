481eabb4a5898d1922a358b0a7d0e624b3a55a6b
Samuel Tech IA <samueldesignerpromax@gmail.com>
2026-03-05 18:54:42 -0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+export default function Home(){
+
+return(
+
+<div>
+
+<h1>Samuel Tech IA</h1>
+
+<p>Vendedor automático com inteligência artificial</p>
+
+<Link href="/login">
+Entrar no sistema
+</Link>
+
+</div>
+
+)
+
+}
